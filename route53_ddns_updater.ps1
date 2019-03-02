eca0c2f53dc2d9a9dfa9a4756d090fdbecd167c7
utsugiriso <utsugiriso@gmail.com>
2019-03-03 00:30:05 +0900

    できるだけAレコードを無駄に更新しないようにした

--- route53_ddns_updater.ps1
+++ route53_ddns_updater.ps1
@@ -6,14 +6,17 @@ $secret_access_key = "XXXX"
 $ip_address = (Invoke-WebRequest "ifconfig.me").Content
 Write-Output($ip_address)
 
-$resourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
-$resourceRecordSet.Name = $domain
-$resourceRecordSet.Type = "A"
-$resourceRecordSet.ResourceRecords = New-Object Amazon.Route53.Model.ResourceRecord($ip_address)
-$resourceRecordSet.TTL = 300
-
-$change = New-Object Amazon.Route53.Model.Change([Amazon.Route53.ChangeAction]::UPSERT, $resourceRecordSet)
-
 $profile_name = "Route53DdnsUpdater"
 Set-AWSCredential -AccessKey $access_key -SecretKey $secret_access_key -StoreAs $profile_name
-Edit-R53ResourceRecordSet -HostedZoneId $hosted_zone_id -ChangeBatch_Change $change -ProfileName $profile_name
+
+if($ip_address -ne ((Get-R53ResourceRecordSet -HostedZoneId $hosted_zone_id -ProfileName $profile_name -StartRecordName $domain -StartRecordType "A").ResourceRecordSets | where Type -eq "A").ResourceRecords.Value){
+    $resourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+    $resourceRecordSet.Name = $domain
+    $resourceRecordSet.Type = "A"
+    $resourceRecordSet.ResourceRecords = New-Object Amazon.Route53.Model.ResourceRecord($ip_address)
+    $resourceRecordSet.TTL = 300
+
+    $change = New-Object Amazon.Route53.Model.Change([Amazon.Route53.ChangeAction]::UPSERT, $resourceRecordSet)
+
+    Edit-R53ResourceRecordSet -HostedZoneId $hosted_zone_id -ChangeBatch_Change $change -ProfileName $profile_name
+}
